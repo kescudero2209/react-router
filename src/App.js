418d5f559590477e475e1bb8f0bf70ebddf36f9d
Karla <kescudero.a@gmail.com>
2022-09-21 21:47:17 -0300

    add link to home with react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Navbar from './components/Navbar'
-import Home from './views/Home'
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
